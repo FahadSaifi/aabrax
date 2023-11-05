53d5b333ed56aa50b8919a8e0d494239a26c15a3
FAHAD SAIFI <fahadsaifi199990@gmail.com>
2023-11-05 12:31:51 +0530

    solutions page added

---
+++ src/data/SolutionData.js
@@ -0,0 +1,48 @@
+import automotivebanner from "../assets/images/solutions/automotivebanner.jpg";
+import automotive from "../assets/images/solutions/automotive.jpg";
+import aslb from "../assets/images/solutions/assemblylinebanner.jpg";
+import asl from "../assets/images/solutions/assemblyline.jpeg";
+import cptb from "../assets/images/solutions/cleaningpretreatmentbanner.jpg";
+import cpt from "../assets/images/solutions/cleaningpretreatment.jpg";
+import dlb from "../assets/images/solutions/diplines.jpg";
+import dl from "../assets/images/solutions/diplinesbanner.jpg";
+
+export const solData = [
+    {
+        id: 1,
+        mainImg: automotivebanner,
+        title: "Automotive Conveyor",
+        tagline: "Automation work and Automation production",
+        secondImg: automotive,
+        secondTitle: "Automotive Conveyor",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. Tristique risus nec feugiat in. Condimentum lacinia quis vel eros donec ac odio tempor. Suspendisse sed nisi lacus sed viverra tellus in hac habitasse. Diam maecenas sed enim ut sem viverra aliquet eget sit. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla. Vel quam elementum pulvinar etiam non quam lacus suspendisse. Sem nulla pharetra diam sit amet nisl. In aliquam sem fringilla ut. Neque aliquam vestibulum morbi blandit cursus risus at. Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit. Purus in mollis nunc sed id.",
+        features: ["Lorem ipsum dolor sit amet, consectetuer adipiscing elit",,"Aliquam tincidunt mauris eu risus.","Lorem ipsum dolor sit amet, consectetuer adipiscing elit","Aliquam tincidunt mauris eu risus.",]
+    },{
+        id: 2,
+        mainImg: aslb,
+        title: "Assembly Line",
+        tagline: "Automation work and Automation production",
+        secondImg: asl,
+        secondTitle: "Assembly Line",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. Tristique risus nec feugiat in. Condimentum lacinia quis vel eros donec ac odio tempor. Suspendisse sed nisi lacus sed viverra tellus in hac habitasse. Diam maecenas sed enim ut sem viverra aliquet eget sit. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla. Vel quam elementum pulvinar etiam non quam lacus suspendisse. Sem nulla pharetra diam sit amet nisl. In aliquam sem fringilla ut. Neque aliquam vestibulum morbi blandit cursus risus at. Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit. Purus in mollis nunc sed id.",
+        features: ["Lorem ipsum dolor sit amet, consectetuer adipiscing elit",,"Aliquam tincidunt mauris eu risus.","Lorem ipsum dolor sit amet, consectetuer adipiscing elit","Aliquam tincidunt mauris eu risus.",]
+    },{
+        id: 3,
+        mainImg: cptb,
+        title: "Cleaning and Preatreatment",
+        tagline: "Automation work and Automation production",
+        secondImg: cpt,
+        secondTitle: "Cleaning and Preatreatment",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. Tristique risus nec feugiat in. Condimentum lacinia quis vel eros donec ac odio tempor. Suspendisse sed nisi lacus sed viverra tellus in hac habitasse. Diam maecenas sed enim ut sem viverra aliquet eget sit. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla. Vel quam elementum pulvinar etiam non quam lacus suspendisse. Sem nulla pharetra diam sit amet nisl. In aliquam sem fringilla ut. Neque aliquam vestibulum morbi blandit cursus risus at. Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit. Purus in mollis nunc sed id.",
+        features: ["Lorem ipsum dolor sit amet, consectetuer adipiscing elit",,"Aliquam tincidunt mauris eu risus.","Lorem ipsum dolor sit amet, consectetuer adipiscing elit","Aliquam tincidunt mauris eu risus.",]
+    },{
+        id: 4,
+        mainImg: dlb,
+        title: "Dip Lines",
+        tagline: "Automation work and Automation production",
+        secondImg: dl,
+        secondTitle: "Dip Lines",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. Tristique risus nec feugiat in. Condimentum lacinia quis vel eros donec ac odio tempor. Suspendisse sed nisi lacus sed viverra tellus in hac habitasse. Diam maecenas sed enim ut sem viverra aliquet eget sit. Rutrum tellus pellentesque eu tincidunt tortor aliquam nulla. Vel quam elementum pulvinar etiam non quam lacus suspendisse. Sem nulla pharetra diam sit amet nisl. In aliquam sem fringilla ut. Neque aliquam vestibulum morbi blandit cursus risus at. Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit. Purus in mollis nunc sed id.",
+        features: ["Lorem ipsum dolor sit amet, consectetuer adipiscing elit",,"Aliquam tincidunt mauris eu risus.","Lorem ipsum dolor sit amet, consectetuer adipiscing elit","Aliquam tincidunt mauris eu risus.",]
+    }
+]
--- src/pages/Solutions.jsx
+++ src/pages/Solutions.jsx
@@ -1,7 +1,58 @@
 import React from "react";
+import { solData } from "../data/SolutionData";
+import { useParams } from "react-router-dom";
+import "../assets/styles/Products.css";
 
-const Solutions = () => {
-  return <div>Solutions</div>;
+const Product = () => {
+  const { id } = useParams();
+
+  const filtData = solData.filter((elm) => {
+    return elm.id == id;
+  });
+
+  return (
+    <>
+      {filtData.map((elm) => (
+        <div className="products-wrapper" key={elm.id}>
+          <div
+            className="prod-heading-wrapper"
+            style={{
+              backgroundImage:
+                "linear-gradient(to right,rgba(0,0,0,0.5), rgba(0,0,0,0.3)) , url(" +
+                elm.mainImg +
+                ")",
+            }}
+          >
+            <h1 className="prod-heading">{elm.title}</h1>
+            <p className="prod-tagline">{elm.tagline}</p>
+          </div>
+          <div className="container">
+            <div className="prod-content">
+              <div className="prod-desc">
+                <div className="prod-desc-left">
+                  <img
+                    src={elm.secondImg}
+                    alt="prod"
+                    className="prod-desc-img"
+                  />
+                </div>
+                <div className="prod-desc-right">
+                  <h2 className="prod-info-title">{elm.secondTitle}</h2>
+                  <p className="prod-info">{elm.desc}</p>
+                </div>
+              </div>
+              <h2 className="prod-features">Features of {elm.secondTitle}</h2>
+              <ul className="prod-list">
+                {elm.features.map((item) => {
+                  return <li key={elm.id}>{item}</li>;
+                })}
+              </ul>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 };
 
-export default Solutions;
+export default Product;
