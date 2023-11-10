3adc5e64a56fc3631e4b750c9d1376a7c3f94aa7
Aman Beg <am2835112@gmail.com>
2023-11-10 17:06:11 +0530

    form api

--- src/components/Homecontact.jsx
+++ src/components/Homecontact.jsx
@@ -2,8 +2,52 @@ import React from 'react';
 import '../assets/styles/Homecontact.css';
 import { IoLocationSharp, IoCall ,} from "react-icons/io5";
 import { FaTelegramPlane, FaGlobeAsia } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Homecontact = () => {
+  const [input, setInput] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    phone: "",
+    msg: "",
+  });
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setInput((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios({
+      method: "post",
+      url: ('https://saifi.co.in/mail.php'),
+      withCredentials: false,
+      header:('Access-Control-Allow-Origin: *'),
+      data: {
+        fname: input.fname,
+        lname: input.lname,
+        email: input.email,
+        phone: input.phone,
+        msg: input.msg
+      }
+    }).then((res)=>{console.log(res)}).catch((error)=>{
+      console.log(error);
+    })
+  };
+
+  useEffect(() => {
+    document.title = "Aabrax | Contact Us";
+  }, []);
   return (
     <>
       <div className="contact-container container">
@@ -37,26 +81,85 @@ const Homecontact = () => {
 
             <td className="contact-form">
               <h3>Get in touch</h3>
-              <form action="">
-                  <div className="name-email">
-                    <div className="name">
-                      <label htmlFor="name">Full Name</label>
-                      <input type="text" id="name" placeholder="Name" required/>
-                    </div>
-                    <div className="email">
-                      <label htmlFor="email">Email Address</label>
-                      <input type="email" id="email" placeholder="Email" required/>
-                    </div>
+              <form onSubmit={handleSubmit}>
+                <div className="name">
+                  <div className="fname">
+                    <label className="contact-labels" htmlFor="fname">
+                      First Name
+                    </label>
+                    <input
+                      className="contact-inputs"
+                      name="fname"
+                      type="text"
+                      id="fname"
+                      placeholder="First name"
+                      onChange={handleChange}
+                      required
+                    />
                   </div>
-                  
-                  <label htmlFor="sub">Subject</label>
-                  <input type="text" id="sub" placeholder="Subject" required/>
+                  <div className="lname">
+                    <label className="contact-labels" htmlFor="lname">
+                      Last Name
+                    </label>
+                    <input
+                      className="contact-inputs"
+                      name="lname"
+                      type="text"
+                      id="lname"
+                      placeholder="Last name"
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+                </div>
+                <div className="email">
+                  <label className="contact-labels" htmlFor="email">
+                    Email Address
+                  </label>
+                  <input
+                    className="contact-inputs"
+                    name="email"
+                    type="email"
+                    id="email"
+                    placeholder="you@company.com"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="phone">
+                  <label className="contact-labels" htmlFor="phone">
+                    Phone Number
+                  </label>
+                  <input
+                    className="contact-inputs"
+                    name="phone"
+                    type="number"
+                    id="phone"
+                    placeholder="+91 1234567890"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="msg">
+                  <label className="contact-labels" htmlFor="msg">
+                    Message
+                  </label>
+                  <textarea
+                    className="contact-inputs"
+                    name="msg"
+                    id="msg"
+                    rows="4"
+                    placeholder="Message"
+                    onChange={handleChange}
+                    required
+                  ></textarea>
+                </div>
 
-                  <label htmlFor="msg">Message</label>
-                  <textarea name="msg" id="msg" rows="5" placeholder="Message" required></textarea>
-
-                  <input type="submit" value="Send Message" />
-
+                <input
+                  className="contact-inputs"
+                  type="submit"
+                  value="Send Message"
+                />
               </form>
             </td>
           </tr>
--- src/pages/Contactus.jsx
+++ src/pages/Contactus.jsx
@@ -1,53 +1,149 @@
 import React from "react";
-import '../assets/styles/Contactus.css';
+import "../assets/styles/Contactus.css";
 import contactImg from "../assets/images/contact.jpg";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Contactus = () => {
-  useEffect(()=>{
-    document.title = "Aabrax | Contact Us"
-  },[])
+  const [input, setInput] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    phone: "",
+    msg: "",
+  });
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setInput((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios({
+      method: "post",
+      url: ('https://saifi.co.in/mail.php'),
+      withCredentials: false,
+      header:('Access-Control-Allow-Origin: *'),
+      data: {
+        fname: input.fname,
+        lname: input.lname,
+        email: input.email,
+        phone: input.phone,
+        msg: input.msg
+      }
+    }).then((res)=>{console.log(res)}).catch((error)=>{
+      console.log(error);
+    })
+  };
+
+  useEffect(() => {
+    document.title = "Aabrax | Contact Us";
+  }, []);
   return (
     <>
-    <div className="container">
+      <div className="container">
         <div className="contact-body">
-            <div className="contact-img">
-              <img src={contactImg} alt="model image" />
-            </div>
-            <div className="contact-form-column">
-              <h3 className="contact-heading">Get in touch</h3>
-              <p className="contact-tagline">Our friendly team would love to hear from you.</p>
-              <form action="">
-                  <div className="name">
-                    <div className="fname">
-                      <label className="contact-labels" htmlFor="fname">First Name</label>
-                      <input className="contact-inputs" name="first-name" type="text" id="fname" placeholder="First name" required/>
-                    </div>
-                    <div className="lname">
-                      <label className="contact-labels" htmlFor="lname">Last Name</label>
-                      <input className="contact-inputs" name="last-name" type="text" id="lname" placeholder="Last name" required/>
-                    </div>
-                  </div>
-                  <div className="email">
-                      <label className="contact-labels" htmlFor="email">Email Address</label>
-                      <input className="contact-inputs" name="email-address"type="email" id="email" placeholder="you@company.com" required/>
-                  </div>
-                  <div className="phone">
-                    <label className="contact-labels" htmlFor="phone">Phone Number</label>
-                    <input className="contact-inputs" name="phone-number" type="number" id="phone" placeholder="+91 1234567890" required/>
-                  </div>
-                  <div className="msg">
-                    <label className="contact-labels" htmlFor="msg">Message</label>
-                    <textarea className="contact-inputs" name="msg" id="msg" rows="4" placeholder="Message" required></textarea>
-                  </div>
+          <div className="contact-img">
+            <img src={contactImg} alt="model image" />
+          </div>
+          <div className="contact-form-column">
+            <h3 className="contact-heading">Get in touch</h3>
+            <p className="contact-tagline">
+              Our friendly team would love to hear from you.
+            </p>
+            <form onSubmit={handleSubmit}>
+              <div className="name">
+                <div className="fname">
+                  <label className="contact-labels" htmlFor="fname">
+                    First Name
+                  </label>
+                  <input
+                    className="contact-inputs"
+                    name="fname"
+                    type="text"
+                    id="fname"
+                    placeholder="First name"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="lname">
+                  <label className="contact-labels" htmlFor="lname">
+                    Last Name
+                  </label>
+                  <input
+                    className="contact-inputs"
+                    name="lname"
+                    type="text"
+                    id="lname"
+                    placeholder="Last name"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+              </div>
+              <div className="email">
+                <label className="contact-labels" htmlFor="email">
+                  Email Address
+                </label>
+                <input
+                  className="contact-inputs"
+                  name="email"
+                  type="email"
+                  id="email"
+                  placeholder="you@company.com"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="phone">
+                <label className="contact-labels" htmlFor="phone">
+                  Phone Number
+                </label>
+                <input
+                  className="contact-inputs"
+                  name="phone"
+                  type="number"
+                  id="phone"
+                  placeholder="+91 1234567890"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="msg">
+                <label className="contact-labels" htmlFor="msg">
+                  Message
+                </label>
+                <textarea
+                  className="contact-inputs"
+                  name="msg"
+                  id="msg"
+                  rows="4"
+                  placeholder="Message"
+                  onChange={handleChange}
+                  required
+                ></textarea>
+              </div>
 
-                  <input className="contact-inputs" type="submit" value="Send Message" />
-              </form>
-            </div>
+              <input
+                className="contact-inputs"
+                type="submit"
+                value="Send Message"
+              />
+            </form>
+          </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Contactus;
